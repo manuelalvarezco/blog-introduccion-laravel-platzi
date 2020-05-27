e1f952e1b51f4b4de6f85fbb94562ec2700a77c8
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2020-05-27 10:16:19 -0500

    avances

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,10 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card mb-4">
-                <div class="card-body">
-                    @if ($post->img)
+                @if ($post->img)
                         <img src="{{ $post->get_img }}" alt="imagen" class="card-img-top">
-                    @elseif($post->iframe)
+                @endif
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    @if ($post->iframe)
                     <div class="embed-responsive embed-responsive-16by9">
 
                         {!! $post->iframe !!}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,8 +6,7 @@
         <div class="col-md-8">
             @foreach ($posts as $post)
             <div class="card mb-4">
-                <div class="card-body">
-                    @if ($post->img)
+                @if ($post->img)
                         <img src="{{ $post->get_img }}" alt="imagen" class="card-img-top">
                     @elseif($post->iframe)
                     <div class="embed-responsive embed-responsive-16by9">
@@ -15,6 +14,8 @@
                         {!! $post->iframe !!}
                     </div>
                     @endif
+                <div class="card-body">
+
                     <h5 class="card-title">{{ $post->title }}</h5>
                     <p class="card-text">
                         {{ $post->get_excerpt }}
